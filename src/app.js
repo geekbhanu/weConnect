6e4945b42f81c02327bc9c7c58a5e98b14d9f4ea
Bhanu <bhanu.sriva25@gmail.com>
2025-07-14 13:54:18 +0530

    multiple route

--- src/app.js
+++ src/app.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const app = express();
+//app.use("/route", rH, [rH2,rH3], rH4, rH5);
 
-// ac, abc
 
-app.get("/user/:userId/:name/:password", (req, res) => {
-    console.log(req.params);
-    
-    res.send({firstName: "Bhanu", lastName: "Srivastava"});
+app.get("/user", (req, res, next)=>{
+    console.log("Handling the route user!!");
+    next();
+    //Route Handler
+    // res.send("Route Handler 1");
 });
+(req, res, next) =>{
+    console.log("Handling the route user 2!!");
+    //res.send("2nd Handler")
+    next();
+},
+(req, res, next) =>{
+    console.log("Handling the route user 3");
+    //res.send("3rd Response")
+    next();
+}
+(req, res, next) =>{
+    console.log("Handling the route user 4");
+    //res.send("4th Response")
+    next();
+}
+(req, res, next) =>{
+    console.log("handling the route user 5");
+    res.send("5th Response")
+}
 app.listen(7777, ()=>{
-    console.log("Server is listening on port 7777...")
+    console.log("Server is successfully listening on port 7777...");
+    
 });
