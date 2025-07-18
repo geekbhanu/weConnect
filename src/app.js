912b4bb7a4f2c02bc42fd9a421996da4eb08b192
Bhanu <bhanu.sriva25@gmail.com>
2025-07-18 12:20:13 +0530

    Schema Validations

--- src/app.js
+++ src/app.js
@@ -69,10 +69,12 @@ app.patch("/user", async (req, res) =>{
     const userId = req.body.userId;
     const data = req.body;
     try{
-        await User.findByIdAndUpdate({_id: userId },data, {returnDocument: "before"});
-        res.send("User updated successfully");
+        const user = await User.findByIdAndUpdate({_id: userId },data, {
+        returnDocument: "afer",
+        runValidators: true,
+        }); // Ensure that the update respects the schema validation rules
     }catch (err){
-        res.status(400).send("Something went wrong")
+        res.status(400).send("Update failed: " + err.message);  
     }
 })
  connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -1,23 +1,53 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 50
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        lowercase: true, // Ensure emailId is stored in lowercase
+        required: true, // Ensure emailId is required
+        unique: true, // Ensure emailId is unique
+        trim: true // Remove any leading or trailing whitespace
     },
     password: {
-        type: String    
+        type: String,
+        required: true,    
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18,
+        max: 50,        
     },
     gender: {
-        type: String
+        type: String,
+        validate(value){
+            if(!["male", "female", "other"].includes(value)){
+                throw new Error("Gender data is not valid");
+        }
+    },
+},
+    photoUrl: {
+        type: String,
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISuukVSb_iHDfPAaDKboFWXZVloJW9XXiwGYFab-QwlAYQ3zFsx4fToY9ijcVNU5ieKk&usqp=CAU" // Default profile picture URL
+    },
+    about: {
+        type: String,
+        default: "This is a default about the user"
+    },
+    skills: {
+        type: [String],
     }
-})
+},
+{
+    timestamps: true // Automatically manage createdAt and updatedAt fields
+}
+);
 // Mongoose Model
 module.exports = mongoose.model("User", userSchema); 
